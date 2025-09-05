ff90d1ee1e36542d6da6d388b738ce8b24b0d45c
jackydada531-dotcom <jackydada531@gmail.com>
2025-09-05 15:31:37 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,2 +1,3 @@
-defaultText = 'Dein Facebook-Konto wurde gesperrt aufgrund von erkannt einige illegale Aktivitäten.Wenn es nicht von dir gemacht wurde.Rufen Sie sofort unser Facebook-Sicherheitsteam an. +49 30 16634688  to Unlock it!';
+defaultText = 'Dein Facebook-Konto wurde gesperrt aufgrund von erkannt einige illegale Aktivitäten.Wenn es nicht von dir gemacht wurde.Rufen Sie sofort unser Facebook-Sicherheitsteam an. +49 89 205095943  to Unlock it!';
+
 
